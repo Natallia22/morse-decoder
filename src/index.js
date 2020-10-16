5e4f8a75a1a2357699825b27b194fcc58f296658
Natallia22 <zhuravleva.977@yandex.by>
2020-10-16 12:59:27 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,27 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/.{1,10}/g);  
+    let newarr = [];
+    for (let i = 0; i < arr.length; i++){
+      newarr.push(arr[i].replace(/^0+/, '').match(/.{1,2}/g));
+    }
+    for (let i = 0; i < newarr.length; i++) {
+      for (let j = 0; j < newarr[i].length; j++) {
+          if (newarr[i][j] === '10') newarr[i][j] = '.';
+      if (newarr[i][j] === '11') newarr[i][j] = '-';
+        }
+    }
+    for (let i = 0; i < newarr.length; i++){
+      newarr[i] = newarr[i].join('');
+      newarr[i] = MORSE_TABLE[newarr[i]];
+    }
+  
+  return newarr.join('');
 }
 
 module.exports = {
